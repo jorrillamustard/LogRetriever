02be7aea7de58e7b1d6a4bd2a431279ef2f6599b
jorrillamustard <merrick.kyle@gmail.com>
2016-03-18 10:57:19 -0500

    Build v1

---
+++ Log_Retriever/WindowsApplication1/zipSettings.designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class zipSettings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(zipSettings))
+        Me.lstOptions = New System.Windows.Forms.ListBox()
+        Me.lstSelected = New System.Windows.Forms.ListBox()
+        Me.btnMove = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.chkSelectAll = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'lstOptions
+        '
+        Me.lstOptions.FormattingEnabled = True
+        Me.lstOptions.ItemHeight = 16
+        Me.lstOptions.Items.AddRange(New Object() {"AnnotationConversionLog", "BusinessServices", "BusinessServicesCommon", "BusinessServicesCommon_Endpoints", "BusinessServicesCommon_EndpointTrace", "BusinessServicesCommon_StabilityMonitoring", "DatabaseUpgrade", "Infrastructure", "IntegrationService", "License", "MAP_log", "Orchestration", "ProcessingHost_error", "ProcessingHost_info", "ProcessingManager_info", "ProductionSetObjectConversionLog", "ProcessingServices", "RIASqlTraceLog", "site_server", "ThreatBridge", "WorkDistribution", "WorkManager"})
+        Me.lstOptions.Location = New System.Drawing.Point(12, 63)
+        Me.lstOptions.Name = "lstOptions"
+        Me.lstOptions.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lstOptions.Size = New System.Drawing.Size(178, 196)
+        Me.lstOptions.TabIndex = 0
+        '
+        'lstSelected
+        '
+        Me.lstSelected.FormattingEnabled = True
+        Me.lstSelected.ItemHeight = 16
+        Me.lstSelected.Location = New System.Drawing.Point(255, 63)
+        Me.lstSelected.Name = "lstSelected"
+        Me.lstSelected.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lstSelected.Size = New System.Drawing.Size(156, 196)
+        Me.lstSelected.TabIndex = 1
+        '
+        'btnMove
+        '
+        Me.btnMove.Location = New System.Drawing.Point(196, 112)
+        Me.btnMove.Name = "btnMove"
+        Me.btnMove.Size = New System.Drawing.Size(53, 23)
+        Me.btnMove.TabIndex = 2
+        Me.btnMove.Text = ">>"
+        Me.btnMove.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(196, 182)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(53, 23)
+        Me.btnRemove.TabIndex = 3
+        Me.btnRemove.Text = "<<"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Available Logs:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(235, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Logs to Archive:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 279)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 27)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Zip Logs"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'chkSelectAll
+        '
+        Me.chkSelectAll.AutoSize = True
+        Me.chkSelectAll.Location = New System.Drawing.Point(265, 285)
+        Me.chkSelectAll.Name = "chkSelectAll"
+        Me.chkSelectAll.Size = New System.Drawing.Size(123, 21)
+        Me.chkSelectAll.TabIndex = 7
+        Me.chkSelectAll.Text = "Select All Logs"
+        Me.chkSelectAll.UseVisualStyleBackColor = True
+        '
+        'zipSettings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 332)
+        Me.Controls.Add(Me.chkSelectAll)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnMove)
+        Me.Controls.Add(Me.lstSelected)
+        Me.Controls.Add(Me.lstOptions)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "zipSettings"
+        Me.Text = "Log Retriever"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstOptions As ListBox
+    Friend WithEvents lstSelected As ListBox
+    Friend WithEvents btnMove As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents chkSelectAll As CheckBox
+End Class
---
+++ Log_Retriever/WindowsApplication1/zipSettings.vb
@@ -0,0 +1,148 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+
+Public Class zipSettings
+    Dim format As String = "M-d-yyyy-HH-mm-ss"
+
+    Private Sub zipSettings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Function checkLog()
+
+        Dim SS As String
+        Dim SS2 As String
+        Dim flag = False
+
+
+        For Each c In "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()
+            If flag = False Then
+                SS = c + ":\Program Files\Resolution1\SiteServer\site_server.log"
+                SS2 = c + ":\Program Files\AccessData\SiteServer\site_server.log"
+                If (Not System.IO.File.Exists(SS)) Then
+                    flag = False
+                Else
+                    flag = True
+                    Return c + ":\Program Files\Resolution1\SiteServer\"
+
+                End If
+
+                If (Not System.IO.File.Exists(SS2)) Then
+                    flag = False
+                Else
+                    flag = True
+                    Return c + ":\Program Files\AccessData\SiteServer\"
+                End If
+            Else
+                GoTo endoffunc
+            End If
+        Next
+
+endoffunc:
+
+    End Function
+
+    Private Function zip()
+        Dim count As Integer = count + 1
+        Dim R1Logs As String = "C:\Users\Public\Documents\Resolution1Logs\"
+        Dim ProcLogs As String = My.Computer.Registry.GetValue(
+    "HKEY_LOCAL_MACHINE\SOFTWARE\AccessData\PStore\EvidenceProcessor", "processingstatedirectory", Nothing)
+        Dim R1Logs2 As String = "C:\Users\Public\Documents\AccessData\"
+        Dim R1SSLogs As String = checkLog()
+        Dim zipPath1 As String = "C:\Users\" + Environment.UserName + "\Documents\" + Now.ToString(format) + "R1Logs" + count.ToString + ".zip"
+        Dim temp As String = "C:\temp\"
+        Dim temp2 As String = temp + "Site_Server.log"
+
+
+
+
+        Try
+            Dim itemsToMove = (From i In lstSelected.Items).ToArray()
+
+            For Each item In itemsToMove
+                If item.ToString = "site_server" Then
+                    For Each f In Directory.GetFiles(R1SSLogs, item + ".*", SearchOption.TopDirectoryOnly)
+                        If File.Exists(f) Then
+                            My.Computer.FileSystem.CopyFile(f, Path.Combine(temp, Path.GetFileName(f)), True)
+                        End If
+                    Next
+                Else
+                    For Each f In Directory.GetFiles(R1Logs, item + ".*", SearchOption.AllDirectories)
+                        If File.Exists(f) Then
+                            My.Computer.FileSystem.CopyFile(f, Path.Combine(temp, Path.GetFileName(f)), True)
+
+                        End If
+                    Next
+                    For Each f In Directory.GetFiles(R1Logs2, item + ".*", SearchOption.AllDirectories)
+                        If File.Exists(f) Then
+                            My.Computer.FileSystem.CopyFile(f, Path.Combine(temp, Path.GetFileName(f)), True)
+                        End If
+                    Next
+                    For Each f In Directory.GetFiles(ProcLogs, item + ".*", SearchOption.AllDirectories)
+                        If File.Exists(f) Then
+                            My.Computer.FileSystem.CopyFile(f, Path.Combine(temp, Path.GetFileName(f)), True)
+                        End If
+                    Next
+                End If
+            Next
+
+            'My.Computer.FileSystem.CopyFile(R1SSLogs, temp2, True)
+
+            ZipFile.CreateFromDirectory(temp, zipPath1)
+            Directory.Delete(temp, True)
+            MessageBox.Show("Archive Created at: " + zipPath1)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+
+        End Try
+    End Function
+
+    Private Sub lstOptions_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstOptions.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnMove_Click(sender As Object, e As EventArgs) Handles btnMove.Click
+
+
+        Dim selectedItems = (From i In lstOptions.SelectedItems).ToArray()
+
+        For Each item In selectedItems
+            lstSelected.Items.Add(item)
+            lstOptions.Items.Remove(item)
+        Next
+
+        lstSelected.EndUpdate()
+
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        Dim selectedItems = (From i In lstSelected.SelectedItems).ToArray()
+
+        For Each item In selectedItems
+            lstOptions.Items.Add(item)
+            lstSelected.Items.Remove(item)
+        Next
+
+        lstSelected.EndUpdate()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        zip()
+
+    End Sub
+
+    Private Sub chkSelectAll_CheckedChanged(sender As Object, e As EventArgs) Handles chkSelectAll.CheckedChanged
+
+        If chkSelectAll.Checked Then
+            lstSelected.Items.AddRange(lstOptions.Items)
+            lstOptions.Items.Clear()
+
+        ElseIf Not chkSelectAll.checked Then
+            lstOptions.Items.AddRange(lstSelected.Items)
+            lstSelected.Items.Clear()
+        End If
+
+
+    End Sub
+End Class
